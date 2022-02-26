2c56313604b50b0171cf25928c05c443d97e22ac
osymandius <bolistevens@gmail.com>
2022-02-26 08:18:14 +0200

    update

--- R/plotting.R
+++ R/plotting.R
@@ -8,9 +8,8 @@ standard_theme <- function() {
           axis.title = element_text(size=14),
           legend.text = element_text(size=12),
           strip.background = element_rect(fill=NA, colour = "white"),
-          panel.background = element_rect(fill=NA, color="black")) +
-    labs(y=element_blank(), x=element_blank())
-    )
+          panel.background = element_rect(fill=NA, color="black"))
+    # labs(y=element_blank(), x=element_blank())
 
 }
 
@@ -22,5 +21,18 @@ every_nth = function(axis_label, n, offset = 0) {
     i <- rep_len(c(TRUE, rep(FALSE, n - 1)), length(axis_label)-offset)
     return(function(x) {x[c(rep(FALSE, offset), i)]})
   }
+
+}
+
+province_grid <- function() {
+  moz_province_grid <- data.frame(
+  code = "",
+  name = c("Cabo Delgado", "Niassa", "Nampula", "Zambézia", "Tete", "Manica", "Sofala", "Inhambane", "Gaza", "Maputo"),
+  row = c(1, 1, 2, 2, 2, 3, 3, 4, 4, 5),
+  col = c(4, 3, 4, 3, 1, 2, 3, 3, 2, 2),
+  stringsAsFactors = FALSE
+)
+
+  facet_geo(~area_name, grid = moz_province_grid)
 
 }
--- R/spectrum.R
+++ R/spectrum.R
@@ -1,41 +1,31 @@
 extract_mozambique_spectrum <- function(areas,
-                                        path = "input/moz_2021_v6.06_18.03.21",
-                                        spec_names = c("Niassa",
-                                                       "Cabo Delgado",
-                                                       "Nampula",
-                                                       "Zambézia",
-                                                       "Tete",
-                                                       "Manica",
-                                                       "Sofala",
-                                                       "Inhambane",
-                                                       "Gaza",
-                                                       "Maputo")) {
-
+         path = "input/moz_2021_v6.06_18.03.21") {
+  
   spec_paths <- gtools::mixedsort(list.files(path, full.names = TRUE))
   dat <- lapply(spec_paths, naomi::extract_pjnz_naomi)
-
-  names(dat) <- spec_names
-
+  
   dat <- dat %>%
-    bind_rows(.id = "area_name")
-
+    bind_rows() %>%
+    separate(spectrum_region_name, sep = "_", into=c(NA, "area_name")) %>%
+    mutate(area_name = ifelse(area_name == "Zambezi", "Zambézia", area_name))
+  
   df <- dat %>%
     left_join(data.frame(age = c(0:80), age_group_start = c(rep(seq(0, 75, 5), each = 5), 80)) %>%
                 dplyr::left_join(naomi::get_age_groups() %>%
                                    dplyr::filter(age_group_span == 5)) %>%
                 dplyr::select(age, age_group)
     )
-
+  
   hiv_indicators <- df %>%
     dplyr::group_by(area_name, year, sex, age_group) %>%
     dplyr::summarise(totpop = sum(totpop),
                      hivpop = sum(hivpop),
-                     artpop = sum(artpop),
+                     artpop_dec31 = sum(artpop_dec31),
                      infections = sum(infections)) %>%
     dplyr::ungroup() %>%
     mutate(source = "Spectrum 2021") %>%
     left_join(areas %>% select(area_id, area_name) %>% st_drop_geometry())
-
+  
   fertility_indicators <- df %>%
     dplyr::filter(sex == "female", age %in% 15:49) %>%
     group_by(area_name, year, age_group, asfr) %>%
@@ -44,21 +34,22 @@ extract_mozambique_spectrum <- function(areas,
            source = "Spectrum 2021") %>%
     ungroup()  %>%
     left_join(areas %>% select(area_id, area_name) %>% st_drop_geometry())
-
+  
   if(nrow(filter(hiv_indicators, is.na(area_id))))
     stop("HIV indicators has areas with no area IDs")
-
+  
   if(nrow(filter(fertility_indicators, is.na(area_id))))
     stop("Fertility indicators has areas with no area IDs")
-
+  
   out <- list()
   out$hiv_indicators <- hiv_indicators
   out$fertility_indicators <- fertility_indicators
-
+  
   out
-
-
+  
+  
 }
+
 
 
 
