9d38f745950be49a25b161ebdb9afcd4b4ae4a8b
osymandius <bolistevens@gmail.com>
2022-08-24 10:17:36 +0100

    Update data_management.R

--- R/data_management.R
+++ R/data_management.R
@@ -59,6 +59,22 @@ five_year_to_15to49 <- function(df, indicators) {
 
 }
 
+single_year_to_five_year <- function(df, fifteen_to_49 = TRUE) {
+  df <- df %>%
+    dplyr::mutate(age_group_label = cut(age, c(0, seq(5, 85, 5)-1), c(paste0(seq(0, 79, 5), "-", seq(5, 80, 5)-1), "80+"), include.lowest=TRUE)) %>%
+    dplyr::left_join(naomi::get_age_groups() %>% select(age_group, age_group_label)) %>%
+    dplyr::select(-age_group_label)
+
+  if(fifteen_to_49) {
+    df %>%
+      dplyr::filter(age %in% 15:49) %>%
+      dplyr::select(-age)
+  } else {
+    df %>%
+      dplyr::select(-age)
+  }
+}
+
 group_proportion <- function(df, variables) {
   df %>%
     dplyr::group_by(across(all_of(variables))) %>%
